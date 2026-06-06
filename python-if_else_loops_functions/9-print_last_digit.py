e93f233ac9daca9eaa6669cb647adb632c1164e3
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-06-06 18:20:12 +0000

    Task 3

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -6,6 +6,6 @@ def print_last_digit(number):
         last_digit = (-number) % 10
     else:
         last_digit = number % 10
-        
+
     print("{}".format(last_digit), end="")
     return last_digit
