680f7d9566e9e1aeab6a7a463abe246c110873dd
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-06-12 09:01:59 +0000

    Task 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,9 +4,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end="")
             count += 1
-        except Exception as e:  # هنا نمسك أي خطأ ونعطيه اسم e
-            print("\n[خطأ]: نوع الخطأ هو:", type(e).__name__)
-            print("[رسالة الخطأ]:", e)
+        except IndexError:
             break
     print("")
     return count
