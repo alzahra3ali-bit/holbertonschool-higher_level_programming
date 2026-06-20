98d2c31d575bc25d8b6d8d19b21cb352863d7a63
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-06-20 13:02:28 +0000

    Task 0

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define a class Square with a private size attribute."""
+
+
+class Square:
+    """A class that defines a square by its size."""
+
+    def __init__(self, size):
+        """Initialize the square with a private size attribute."""
+        self.__size = size
