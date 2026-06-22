8554c2599fe021ac7e4e8b77d037689838c3bfcb
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-06-22 13:35:26 +0000

    Task 1

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define a class BaseGeometry based on 5-base_geometry.py."""
+
+
+class BaseGeometry:
+    """A class representing base geometry."""
+
+    def area(self):
+        """Raise an Exception indicating the method is not implemented."""
+        raise Exception("area() is not implemented")
