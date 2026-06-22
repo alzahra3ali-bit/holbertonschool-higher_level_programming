3a1a7bc741da783c06eb6bf3e26d5b77ea87e899
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-06-22 13:26:03 +0000

    Task 1

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Define a function that checks if an object is a strict subclass."""
+
+
+def inherits_from(obj, a_class):
+    """Check if an object is an instance of a class that inherited from a class.
+
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to match against.
+
+    Returns:
+        bool: True if obj is an instance of a subclass of a_class,
+              otherwise False.
+    """
+    return isinstance(obj, a_class) and type(obj) is not a_class
