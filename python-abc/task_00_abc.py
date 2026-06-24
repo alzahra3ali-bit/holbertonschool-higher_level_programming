e8a66cdd9d25d2cee5f372bd1f7f69691c96c81c
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-06-24 13:11:11 +0000

    Task 0

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -1,11 +1,30 @@
-from abc import ABCmeta,abstracmethod
+#!/usr/bin/env python3
+"""
+Module for abstract animal classes
+"""
+from abc import ABC, abstractmethod
+
+
 class Animal(ABC):
-    @abstracmethod
+    """Abstract class representing an animal"""
+
+    @abstractmethod
     def sound(self):
+        """Abstract method to get the animal's sound"""
         pass
+
+
 class Dog(Animal):
+    """Subclass representing a dog"""
+
     def sound(self):
+        """Returns the sound of a dog"""
         return "Bark"
+
+
 class Cat(Animal):
+    """Subclass representing a cat"""
+
     def sound(self):
+        """Returns the sound of a cat"""
         return "Meow"
