d3fb2a7c422094beac0368ed83f91abad3b2c707
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-06-29 14:07:56 +0000

    Task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-""defind function that Open file""
+"""defind function that Open file"""
 def read_file(filename=""):
-    ""read And Print the file""
-    with open(zahra,"r", encoding="UTF8") as f:
-      print(f.read(), end="")
+    """read And Print the file"""
+    with open(filename,"r", encoding="UTF8") as f:
+        print(f.read(), end="")
