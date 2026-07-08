82b604b123c52d1cd2c1b495d6b27e8ca89fb12c
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-07-08 16:48:03 +0200

    Task 2

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -3,7 +3,7 @@ import requests
 import csv
 def fetch_and_print_posts():
     req = requests.get('https://jsonplaceholder.typicode.com/posts')
-    print("Status Code: ", req.status_code)
+    print("Status Code:", req.status_code)
     if (req.status_code == 200):
       postes = req.json()
       for post in postes:
