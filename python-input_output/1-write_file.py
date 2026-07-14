65fe5c83c715b0266aa63121440374fe747c83b7
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-07-14 16:49:39 +0300

    Task 1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Module to write a string to a text file and return character count."""
+
+
 def write_file(filename="", text=""):
+    """Write a string to a text file (UTF8) and return character count."""
     with open(filename,'w',encoding='utf-8') as f:
         return f.write(text)
 
