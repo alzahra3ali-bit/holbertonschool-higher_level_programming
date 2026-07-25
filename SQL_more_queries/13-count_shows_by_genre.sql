bd121d13e1b773fa4665cb4d453adf65024184aa
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-07-25 21:52:26 +0300

    s

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- Lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
+SELECT genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
+FROM genres
+INNER JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
+GROUP BY genres.id
+ORDER BY number_of_shows DESC;
