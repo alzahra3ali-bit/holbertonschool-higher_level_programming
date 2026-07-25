683fb2fd1f5c1be345d3607fb026563192b7ea95
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-07-25 22:13:56 +0300

    s

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument passed to the script.
+"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cur = db.cursor()
+    # استخدام format والمطابقة الحساسة لحالة الأحرف BINARY
+    query = "SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id ASC"\
+        .format(sys.argv[4])
+    cur.execute(query)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
